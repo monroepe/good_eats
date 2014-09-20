683452b7e1c409c69e52dbce6785611edaef8c83
Peter <monroepe@gmail.com>
2014-09-20 18:45:50 -0400

    Pages added for each restaurant and to add new

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,9 +1,33 @@
 class RestaurantsController < ApplicationController
   def index
-
+    @restaurants = Restaurant.all
   end
 
   def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    respond_to do |format|
+      if @restaurant.save
+        format.html { redirect_to @restaurant}
+      else
+        format.html { render action: 'new' }
+      end
+    end
 
   end
+
+
+  private
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :city, :state, :zip_code, :description, :category)
+  end
+
 end
