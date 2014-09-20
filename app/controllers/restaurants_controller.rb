066592cf19aca07d0f95c4d71a121d3979324647
Peter <monroepe@gmail.com>
2014-09-20 10:28:01 -0400

    Added some of the scaffolding

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,9 @@
+class RestaurantsController < ApplicationController
+  def index
+
+  end
+
+  def show
+
+  end
+end
---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,9 @@
+class ReviewsController < ApplicationController
+  def index
+
+  end
+
+  def show
+
+  end
+end
