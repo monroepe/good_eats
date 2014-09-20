17dc39c678aa52b8f39acde383b5525f7101a6a3
Peter <monroepe@gmail.com>
2014-09-20 19:09:13 -0400

    Page to edit restaurant and link to dlete

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -24,6 +24,30 @@ class RestaurantsController < ApplicationController
 
   end
 
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+
+    if @restaurant.update(restaurant_params)
+      redirect_to @restaurant
+    else
+      render 'edit'
+    end
+
+  end
+
+  def destroy
+
+    @restaurant = Restaurant.find(params[:id])
+
+    @restaurant.destroy
+
+    redirect_to restaurants_path
+
+  end
 
   private
   def restaurant_params
