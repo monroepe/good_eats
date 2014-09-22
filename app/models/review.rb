e9bf4bb87c3893d968dd88a44f16432845f63322
Peter <monroepe@gmail.com>
2014-09-21 21:48:32 -0400

    Welcome page displays 10 most recent restaurants

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,6 @@ class Review < ActiveRecord::Base
   belongs_to :restaurant
 
   validates :rating, numericality: true
-  validates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5}
+  validates :rating, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
   validates :rating, :body, :restaurant_id, presence: true
 end
