ea4fc36fb1184263e96e8b414a645c0cba05a6d9
Peter <monroepe@gmail.com>
2014-09-21 20:35:03 -0400

    Validations added

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ActiveRecord::Base
   has_many :reviews
 
+  validates :zip_code, numericality: true
   validates :name, :address, :city, :state, :zip_code, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,7 @@
 class Review < ActiveRecord::Base
   belongs_to :restaurant
 
+  validates :rating, numericality: true
+  validates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5}
   validates :rating, :body, :restaurant_id, presence: true
 end
